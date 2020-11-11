721bf4877bfef1f990ca806d677447a2b0ae35b7
Justin Marrington <justinmarrington@hey.com>
2020-11-11 15:32:41 +1000

    Initial bootstrapped CRA client and Express server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+// Static asset serving for create-react-app client build, in prod only
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+}
+
+// Read port from ENV first, to protect 12-factor
+app.set('port', process.env.API_PORT || 3080);
+
+app.listen(app.get('port'), () => {
+  console.log(`API server live on http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
